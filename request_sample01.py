2f28a5de481beb78e9bdbe4d6581374570ca7953
Koichiro <koichiro_takagi@outlook.jp>
2019-09-18 21:19:30 +0900

    県名をサーバーから取得することができました。 郵便番号もつけたよ

--- request_sample01.py
+++ request_sample01.py
@@ -5,9 +5,11 @@ response_01 = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=
 data_01 = response_01.json()
 
 print(data_01["results"][0]["address1"])
+print(data_01["results"][0]["zipcode"])
 
 response_02 = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=5670062")
 
 data_02 = response_02.json()
 
 print(data_02["results"][0]["address1"])
+print(data_02["results"][0]["zipcode"])
