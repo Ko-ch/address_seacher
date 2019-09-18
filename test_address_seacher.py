defef647536cb66c408fa8a999c853da4b3d2382
Koichiro <koichiro_takagi@outlook.jp>
2019-09-18 23:08:00 +0900

    リファクタリングを行い、address_searcher.pyを作成（移動）しました。全体検索からMoveを選択すればリファクタリングを自動で行ってくれます。PyCharmすごいっ！

--- test_address_seacher.py
+++ test_address_seacher.py
@@ -1,16 +1,6 @@
 import unittest
 
-import requests
-
-
-class AddressSearcher:
-    def search(self, postal_code):
-        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + postal_code
-        response_dict = requests.get(url)
-        都道府県 = response_dict.json()["results"][0]["address1"]
-        市区町村 = response_dict.json()["results"][0]["address2"]
-        町域 = response_dict.json()["results"][0]["address3"]
-        return f"{都道府県}{市区町村}{町域}"
+from address_seacher import AddressSearcher
 
 
 class TestAddressSearcher(unittest.TestCase):
