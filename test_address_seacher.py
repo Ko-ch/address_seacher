b95dbcd4fb978a1dc2e31ce87fd70931d285bb0e
Koichiro <koichiro_takagi@outlook.jp>
2019-09-18 22:04:07 +0900

    ２つ目のケースを仮実装し、片方のテストケースを落ちたことを確認した。

--- test_address_seacher.py
+++ test_address_seacher.py
@@ -13,6 +13,12 @@ class TestAddressSearcher(unittest.TestCase):
 
         self.assertEqual("岩手県八幡平市大更", actual)
 
+    def test_郵便番号を入力し東京都練馬区豊玉南を取得する(self):
+        address_searcher = AddressSearcher()
+        actual = address_searcher.search(postal_code="1760014")
+
+        self.assertEqual("東京都練馬区豊玉南", actual)
+
 
 if __name__ == "__main__":
     unittest.main()
