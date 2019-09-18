673097507d2093b7c7ba1a508c405f83e60086e2
Koichiro <koichiro_takagi@outlook.jp>
2019-09-18 23:11:28 +0900

    リファクタリング：小変更

--- address_seacher.py
+++ address_seacher.py
@@ -4,8 +4,8 @@ import requests
 class AddressSearcher:
     def search(self, postal_code):
         url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + postal_code
-        response_dict = requests.get(url)
-        都道府県 = response_dict.json()["results"][0]["address1"]
-        市区町村 = response_dict.json()["results"][0]["address2"]
-        町域 = response_dict.json()["results"][0]["address3"]
+        response_dict = requests.get(url).json()
+        都道府県 = response_dict["results"][0]["address1"]
+        市区町村 = response_dict["results"][0]["address2"]
+        町域 = response_dict["results"][0]["address3"]
         return f"{都道府県}{市区町村}{町域}"
