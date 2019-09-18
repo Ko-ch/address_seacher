4e9a97a0e0e54f3dd3a1a458fe6fa8c3f8d4c18f
Koichiro <koichiro_takagi@outlook.jp>
2019-09-18 22:45:08 +0900

    郵便番号から住所を取得する、AddressSearcherクラスのsearchメソッドを実装した。

--- test_address_seacher.py
+++ test_address_seacher.py
@@ -1,9 +1,16 @@
 import unittest
 
+import requests
+
 
 class AddressSearcher:
     def search(self, postal_code):
-        return "岩手県八幡平市大更"
+        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + postal_code
+        response_dict = requests.get(url)
+        都道府県 = response_dict.json()["results"][0]["address1"]
+        市区町村 = response_dict.json()["results"][0]["address2"]
+        町域 = response_dict.json()["results"][0]["address3"]
+        return f"{都道府県}{市区町村}{町域}"
 
 
 class TestAddressSearcher(unittest.TestCase):
@@ -18,6 +25,12 @@ class TestAddressSearcher(unittest.TestCase):
         actual = address_searcher.search(postal_code="1760014")
 
         self.assertEqual("東京都練馬区豊玉南", actual)
+
+    def test_郵便番号を入力し大阪府茨木市東福井を取得する(self):
+        address_searcher = AddressSearcher()
+        actual = address_searcher.search(postal_code="5670062")
+
+        self.assertEqual("大阪府茨木市東福井", actual)
 
 
 if __name__ == "__main__":
