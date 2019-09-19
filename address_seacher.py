d2eac5e20bba876f4528c58de256770c3b1c579e
Koichiro <koichiro_takagi@outlook.jp>
2019-09-19 22:38:53 +0900

    リファクタリング：self.base_urlを作成しました

--- address_seacher.py
+++ address_seacher.py
@@ -2,10 +2,17 @@ import requests
 
 
 class AddressSearcher:
+    def __init__(self):
+        self.base_url = "http://zipcloud.ibsnet.co.jp/api/search"
+
+
     def search(self, postal_code):
-        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + postal_code
+        url = f"{self.base_url}?zipcode={postal_code}"
+
         response_dict = requests.get(url).json()
+
         都道府県 = response_dict["results"][0]["address1"]
         市区町村 = response_dict["results"][0]["address2"]
         町域 = response_dict["results"][0]["address3"]
+
         return f"{都道府県}{市区町村}{町域}"
